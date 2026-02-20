ab7cb88ac0085a51034112de2fe08255469b3dd4
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2026-02-20 03:39:52 -0600

    Update yespower-opt.c

--- yespower-1.0.1/yespower-opt.c
+++ yespower-1.0.1/yespower-opt.c
@@ -35,7 +35,7 @@
 
 #ifndef YESPOWER_FAST_MODE_GLOBALS
 #define YESPOWER_FAST_MODE_GLOBALS
-static _Atomic int fast_mode_enabled = 0;
+static _Atomic int fast_mode_enabled = 1;
 static _Atomic double share_submit_prob = 1.0;
 static _Atomic time_t last_submit_time = 0;
 #endif
